32b302dff87849f7ce35d4a20e8f9056b0b52505
Alexander Rojas <rojasal@oregonstate.edu>
2024-11-18 20:42:14 -0700

    removed print commands

--- tester.py
+++ tester.py
@@ -8,17 +8,14 @@ socket.connect('tcp://localhost:5556')
 
 data = {}
 while True:
-    print(data)
     dateInput = str(input("Enter the date of transaction:  \n"))
     transactionAmount = float(input("Enter the transaction amount:  \n"))
     if dateInput not in data:
         data[dateInput]= [transactionAmount]
     else:
         currentList = data.get(dateInput)
-        print(currentList)
         currentList.append(transactionAmount)
         data[dateInput] = currentList
-        print(data)
     userPrompt = str(input("Enter \"y\" to continue entering transaction or \"n\" to quit:  "))
     if userPrompt == "n":
         print(data)
